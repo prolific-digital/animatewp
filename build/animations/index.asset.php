f03b34cca09dfdd9f0b24a655eb3f6f8da73824e
Chris Miller <miller.t.chris@gmail.com>
2024-07-28 22:42:30 -0400

    Fixed issue with gravity forms

--- build/animations/index.asset.php
+++ build/animations/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '8e91b3c06f2cef396c00');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'f408edaddadc663a9429');
